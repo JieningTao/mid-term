59ef32464e539c36c980c133df8f94aeeb585404
Jiening Tao <Jiening.Tao@iam.local>
2018-09-18 15:18:50 -0500

    partial move script completed

--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
+    public Rigidbody2D Thisrigidbody;
 
 	// Use this for initialization
 	void Start () {
 		
+
+
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Thisrigidbody.velocity = new Vector2(1, Thisrigidbody.velocity.y);
+        }
+        
+
+
+    }
 }
