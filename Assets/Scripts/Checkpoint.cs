0cee2a21a9f3548838a079af57f0f5b6fd5c763f
Jiening Tao <Jiening.Tao@iam.local>
2018-11-20 15:14:42 -0600

    added all scripts

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private Color inactiveColor, activeColor;
+
+
+    private AudioSource audioSource;
+    private bool isactivated = false;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 shape;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        shape = transform.localScale;
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void UpdateColor()
+    {
+        Color color = inactiveColor;
+        if (isactivated)
+            color = activeColor;
+
+        spriteRenderer.color = color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+            if (collision.gameObject.CompareTag("Player") && !isactivated)
+            {
+                Debug.Log("Check Point Reached!");
+                PlayerMoveScript player = collision.GetComponent<PlayerMoveScript>();
+                player.setcurrentcheckpoint(this);
+            
+                audioSource.Play();
+            }
+        
+    }
+
+    public void setisactivated(bool value)
+    {
+        isactivated = value;
+        UpdateColor();
+    }
+}
---
+++ Assets/Scripts/pickupGem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupGem : MonoBehaviour
+{
+
+    [SerializeField]
+    private Collider2D playercollider;
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        playercollider = GetComponent<PolygonCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
