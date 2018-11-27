156653a6c25f5d6cb40cb88bd5654aa2830bb509
JieningTao <43178991+JieningTao@users.noreply.github.com>
2018-11-27 15:02:39 -0600

    Update CameraFollowScript.cs

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowScript : MonoBehaviour {
+public class CameraFollowScript : MonoBehaviour 
+{
 
     [SerializeField]
     private GameObject Player;
