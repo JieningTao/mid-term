9aef44ffd74896636d101137161704733e454579
JieningTao <43178991+JieningTao@users.noreply.github.com>
2018-11-27 15:03:01 -0600

    Update CameraFollowScript.cs

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class CameraFollowScript : MonoBehaviour 
 {
-
     [SerializeField]
     private GameObject Player;
     [SerializeField]
     private int viewdistance;
-
-
     private Vector2 offest;
-
     // Use this for initialization
     void Start ()
     {
         offest = transform.position - Player.transform.position;
     }
-	
 	// Update is called once per frame
 	void Update ()
     {
