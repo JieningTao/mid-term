b564460b5b7d82325bcd99415c5f6bc717ada89f
JieningTao <43178991+JieningTao@users.noreply.github.com>
2018-11-27 15:07:51 -0600

    Update Hazard.cs

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -14,9 +14,6 @@ public class Hazard : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        
-
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("player killed!");
