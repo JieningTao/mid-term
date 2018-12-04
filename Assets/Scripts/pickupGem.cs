f490e4187d19cffb8e67910a9afa69ba569479d7
JieningTao <43178991+JieningTao@users.noreply.github.com>
2018-12-04 12:44:58 -0600

    accepted changes.

--- Assets/Scripts/pickupGem.cs
+++ Assets/Scripts/pickupGem.cs
@@ -8,18 +8,12 @@ public class pickupGem : MonoBehaviour
     [SerializeField]
     private Collider2D playercollider;
 
-
-
-
-    // Use this for initialization
     void Start ()
     {
         playercollider = GetComponent<PolygonCollider2D>();
-	}
-	
-	// Update is called once per frame
+    }
+    
 	void Update ()
-    {
-		
-	}
+       {
+       }
 }
