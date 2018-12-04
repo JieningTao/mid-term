146cbdfe56c0653805b2cab71364e6e1a7cd08a3
JieningTao <43178991+JieningTao@users.noreply.github.com>
2018-12-04 14:00:10 -0600

    accepted changes

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -30,7 +30,7 @@ public class Checkpoint : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-            if (collision.gameObject.CompareTag("Player") && !isactivated)
+        if (collision.gameObject.CompareTag("Player") && !isactivated)
             {
                 Debug.Log("Check Point Reached!");
                 PlayerMoveScript player = collision.GetComponent<PlayerMoveScript>();
